ed6534e582a9c825ce6f47467fc3a360f06e968d
Evgeny Danilchenko <evdanil@gmail.com>
2025-03-19 05:45:53 +0000

    Update source code for release 0.1.150

--- cn-tool.py
+++ cn-tool.py
@@ -59,7 +59,7 @@ from rich._emoji_codes import EMOJI
 del EMOJI["cd"]
 
 MIN_INPUT_LEN = 5
-version = '0.1.149 hash 73e1312'
+version = '0.1.150 hash 9524788'
 
 # increment cache_version during release if indexes or structures changed and rebuild of the cache is required
 cache_version = 2
